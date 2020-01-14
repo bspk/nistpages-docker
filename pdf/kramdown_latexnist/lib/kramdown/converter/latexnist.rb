019fafd8e3e81ff5dcaac7d60b18495e4c361780
Justin Richer <justin@bspk.io>
2020-01-14 16:28:10 -0500

    inverted table headers

--- pdf/kramdown_latexnist/lib/kramdown/converter/latexnist.rb
+++ pdf/kramdown_latexnist/lib/kramdown/converter/latexnist.rb
@@ -10,18 +10,18 @@ module Kramdown
 		#
 		class Latexnist < Latex
 			
-	        # Initialize the LaTeX converter with the +root+ element and the conversion +options+.
+			# Initialize the LaTeX converter with the +root+ element and the conversion +options+.
 			def initialize(root, options)
 				@headers = options[:yaml_headers]
 				super
-		        ENTITY_CONV_TABLE.each_value {|v| v[0] = "#{v[0]}{}" } # add extra escape sequence to HTML entity conversion values
+				ENTITY_CONV_TABLE.each_value {|v| v[0] = "#{v[0]}{}" } # add extra escape sequence to HTML entity conversion values
 			end
 
-	        def convert_footnote(el, opts)
-	          @data[:packages] << 'fancyvrb'
-			  options = opts.dup.merge(:footnote => true) #flag everythign inside a footnote as special because we need to treat things a little differently sometimes
-	          "\\footnote{#{inner(el.value, options).rstrip}}"
-	        end
+			def convert_footnote(el, opts)
+				@data[:packages] << 'fancyvrb'
+				options = opts.dup.merge(:footnote => true) #flag everythign inside a footnote as special because we need to treat things a little differently sometimes
+				"\\footnote{#{inner(el.value, options).rstrip}}"
+			end
 			
 			def convert_a(el, opts)
 				url = el.attr['href']
@@ -41,28 +41,34 @@ module Kramdown
 				end
 			end
 			
-	        def convert_img(el, _opts)
-	          line = el.options[:location]
+			def convert_img(el, _opts)
+				line = el.options[:location]
 			  
-			  src = el.attr['src']
+				src = el.attr['src']
 			  
-	          if src =~ /^(https?|ftps?):\/\//
-	            warning("Cannot include non-local image#{line ? " (line #{line})" : ''}")
-	            ''
-	          elsif !src.empty?
-	            @data[:packages] << 'graphicx'
+				if src =~ /^(https?|ftps?):\/\//
+					warning("Cannot include non-local image#{line ? " (line #{line})" : ''}")
+					''
+				elsif !src.empty?
+					@data[:packages] << 'graphicx'
 				
-				# grab our special image path if it's there
-				src = el.attr['latex-src'] || src
+					# grab our special image path if it's there
+					src = el.attr['latex-src'] || src
 				
-	            "#{latex_link_target(el)}\\includegraphics[width=\\linewidth]{#{src}}"
-	          else
-	            warning("Cannot include image with empty path#{line ? " (line #{line})" : ''}")
-	            ''
-	          end
-	        end
+					"#{latex_link_target(el)}\\includegraphics[width=\\linewidth]{#{src}}"
+				else
+					warning("Cannot include image with empty path#{line ? " (line #{line})" : ''}")
+					''
+				end
+			end
 
-	        TABLE_ALIGNMENT_CHAR = {default: 'L', left: 'L', center: 'C', right: 'R'} # :nodoc:
+			TABLE_ALIGNMENT_CHAR = {default: 'L', left: 'L', center: 'C', right: 'R'} # :nodoc:
+
+
+			# \begin{tabulary}{\textwidth}{|p{0.1\textwidth}|p{0.07\textwidth}|p{0.25\textwidth}|p{0.2\textwidth}|p{0.2\textwidth}|}   %  latex-columns="p@0.1\textwidth,p@0.07\textwidth,p@0.25\textwidth,p@0.2\textwidth,p@0.2\textwidth"
+			# \hline\rowcolor{black}
+			# \color{white}\RaggedRight Color & \color{white}\RaggedRight Zone &\color{white}\RaggedRight sRGB Tristimulus Value (IEC 61966-2-1) &\color{white}\RaggedRight sRGB Value (IEC 61966-2-1) &\color{white}\RaggedRight CMYK Value \{C,M,Y,K\}\\ \hline
+			
 
 			def convert_table(el, opts)
 				@data[:packages] << 'tabulary'
@@ -70,7 +76,7 @@ module Kramdown
 				
 				if el.attr['latex-columns']
 					# input is col@width,col@width,...
-					# output is col{width\textwidth}|col{width}|...
+					# output is col{width}|col{width}|...
 					align = el.attr['latex-columns'].split(',').map { |a| a.split('@') }.map { |c,w| c + '{' + w + '}' }.join('|')
 				else
 					align = el.options[:alignment].map {|a| TABLE_ALIGNMENT_CHAR[a] }.join('|')
@@ -85,9 +91,31 @@ module Kramdown
 				end
 			end
 
-	        def convert_tr(el, opts)
-	          el.children.map {|c| send("convert_#{c.type}", c, opts) }.join(' & ') << "\\\\ \\hline\n"
-	        end
+			def convert_tr(el, opts)
+				if opts[:thead]
+					rowflags = "\\rowcolor{black}"
+				else
+					rowflags = ""
+				end
+				
+				"#{rowflags}\n" << \
+				el.children.map {|c| send("convert_#{c.type}", c, opts) }.join(' & ') << "\\\\ \\hline\n"
+			end
+
+			def convert_td(el, opts)
+				if opts[:thead]
+					# table header
+					"\\color{white}\\RaggedRight #{inner(el, opts)}"
+				else
+					# table body
+					inner(el, opts)
+				end
+			end
+			
+			def convert_thead(el, opts)
+				options = opts.dup.merge(:thead => true) #flag everythign inside as part of a table header
+				"#{inner(el, options)}\n"
+			end
 
 			# Debug helper method
 			def printelopts(el, opts)
